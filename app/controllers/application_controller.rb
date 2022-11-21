6465003ac7217b1347d680ee3c49f9bcb98a6ec8
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-21 07:48:00 +0700

    SiGG-Ruby-client-app-controllers-application_controller-rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,42 +1,42 @@
 class ApplicationController < ActionController::Base
-  include Robinhood
+  include Wallet
   protect_from_forgery with: :exception
 
   before_filter :check_for_new_user
 
-  def user_logged_in_to_robinhood?
-    @logged_in ||= session[:robinhood_oauth].present? && session[:robinhood_id].present?
+  def user_logged_in_to_Wallet?
+    @logged_in ||= session[:Wallet_oauth].present? && session[:Wallet_id].present?
   end
 
   def current_user
-    @current_user ||= RobinhoodUser.find_by robinhood_id: session[:robinhood_id] if user_logged_in_to_robinhood?
+    @current_user ||= WalletUser.find_by Wallet_id: session[:Wallet_id] if user_logged_in_to_Wallet?
   end
 
   def find_or_create_instrument url
     instrument = Instrument.find_by url: url
     if instrument.nil?
-      instrument_data =  robinhood_get url
+      instrument_data =  Wallet_get url
       instrument = Instrument.create!(
                                       name: instrument_data["name"],
                                       url: instrument_data["url"],
                                       quote_url: instrument_data["quote"],
                                       symbol: instrument_data["symbol"],
                                       fundamentals_url: instrument_data["fundamentals"],
-                                      robinhood_id: instrument_data["id"]
+                                      Wallet_id: instrument_data["id"]
                                       )
     end
     instrument
   end
 
   def find_or_create_crypto_pair pair_id
-    instrument = Instrument.find_by robinhood_id: pair_id
+    instrument = Instrument.find_by Wallet_id: pair_id
     if instrument.nil?
       data = get_crypto_pair pair_id
       instrument = Instrument.create!(
                                       name: data["name"],
                                       symbol: data["symbol"],
-                                      robinhood_id: data["id"],
-                                      url: "#{ROBINHOOD_CRYPTO_URL}/currency_pairs/#{data["id"]}/"
+                                      Wallet_id: data["id"],
+                                      url: "#{Wallet_CRYPTO_URL}/currency_pairs/#{data["id"]}/"
                                       )
     end
     instrument
@@ -46,10 +46,10 @@ class ApplicationController < ActionController::Base
 
   def check_for_new_user
     if current_user.nil?
-      return unless user_logged_in_to_robinhood?
+      return unless user_logged_in_to_Wallet?
       get_user
-      user = RobinhoodUser.create!(
-                                   robinhood_id: @user["id"],
+      user = WalletUser.create!(
+                                   Wallet_id: @user["id"],
                                    username: @user["username"],
                                    first_name: @user["first_name"],
                                    last_name: @user["last_name"]
@@ -57,7 +57,7 @@ class ApplicationController < ActionController::Base
       
       # load accounts
       get_accounts.each do |a|
-        current_user.robinhood_accounts.create! account_number: a["account_number"]
+        current_user.Wallet_accounts.create! account_number: a["account_number"]
       end
       
       # load portfolio
@@ -72,7 +72,7 @@ class ApplicationController < ActionController::Base
       # load watchlist
       get_watchlists
       default_watchlist = @watchlists.first
-      default_watchlist_data = robinhood_get default_watchlist["url"]
+      default_watchlist_data = Wallet_get default_watchlist["url"]
       instruments = []
       get_all_results(default_watchlist_data).each do |stock|
         instrument = find_or_create_instrument stock["instrument"]
